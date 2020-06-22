f9c6db65af9e23ac0277781afce65f5e924acfde
yoimapren <yoimapren27@gmail.com>
2020-06-23 00:28:37 +0700

    gallery hehe

---
+++ gallery/app/settings.php
@@ -0,0 +1,9 @@
+<?php
+$settings['db_host'] = 'localhost';
+$settings['db_name'] = 'albacadabra_gallery';
+$settings['db_user'] = 'albacadabra_admin';
+$settings['db_pass'] = 'Abcd_2018';
+$settings['db_table_prefix'] = 'chv_';
+$settings['db_driver'] = 'mysql';
+$settings['db_pdo_attrs'] = [];
+$settings['debug_level'] = 1;
